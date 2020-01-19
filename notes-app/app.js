533d897699f8c3125dbdffe5a6f3df770eba637f
rrabit42 <gegiraffe@naver.com>
2020-01-19 21:41:27 +0900

    Importing npm Modules 2(강의필기 포함)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,21 +1,26 @@
-const add = require('./utils.js') //then utils.js runs too~
-//인자를 export해주니까 받아야함!
+/*
++)우리는 ES7임. No ES6
 
-const sum = add(4,-2)
+npm i validator하면(i는 install의 축약어)
+going to the NPM server로 가서 모든 code를 grab해옴
+그러면 두가지 일이 일어나는데
+package-lock.json파일을 얻게 되고
+new directory인 node_modules를 얻게 됨
+node_moduels는 contains all of the code for the dependencies we installed.
 
-//그냥 console.log(add(4,-2))해도 됨!
-console.log(sum)
+package-lock.json은 contains extra information making NPM a bit faster and a bit more secure.
+It lists out exact versions of all of our dependencies as well as where they were fetched from. (resolved)
+And we also have a char hash making sure that we're getting the exact code that we got previously if we were to install a dependency again. (integrity)
 
-//
-//Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing message to console
+*/
 
+const validator = require('validator') //npm moduels은 상대 경로 안써도 됨
+const add = require('./utils.js')
 
-const getNotes = require('./notes.js')
+//npm 홈페이지에 그 모듈 사용법 많이 나와있음~
 
-console.log(getNotes());
-//console.log(getNotes)하면 [Function: getNotes]가 출력됨! 이게 뭘까?!
+console.log(validator.isEmail('andrew@example.com')) //answer is 'true'
+
+console.log(validator.isEmail('example.com'))
+
+console.log(validator.isURL('example.com'))
