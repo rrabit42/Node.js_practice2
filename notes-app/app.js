2b8c8622c3ad76dbfbe2ee819203a80ab80d909f
rrabit42 <gegiraffe@naver.com>
2020-01-19 21:09:05 +0900

    Importing Your Own Files - Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,3 +5,17 @@ const sum = add(4,-2)
 
 //그냥 console.log(add(4,-2))해도 됨!
 console.log(sum)
+
+//
+//Challenge: Define and use a function in a new file
+//
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console
+
+
+const getNotes = require('./notes.js')
+
+console.log(getNotes());
+//console.log(getNotes)하면 [Function: getNotes]가 출력됨! 이게 뭘까?!
---
+++ notes-app/notes.js
@@ -0,0 +1,6 @@
+const getNotes = function(){
+  return 'Your notes...'
+}
+
+//이거 빼먹어서 틀림!! 잊지 말기~
+module.exports = getNotes
