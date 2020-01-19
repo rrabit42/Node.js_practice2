c94a1e832a16b1f7dfcb638fdddfcba195cdc4fc
rrabit42 <gegiraffe@naver.com>
2020-01-19 20:52:18 +0900

    Importing Node.js Core Modules - Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,3 +5,11 @@ const fs = require('fs') // fs module is stored in variable
 //http://nodejs.org/dist/latest-v11.x/docs/api/
 //read official docs
 fs.writeFileSync('notes.txt', 'My name is Andrew.')
+
+//
+// Challenge: Append a message to notes.txt
+//
+// 1. User appendFileSync to append to the file(sync가 붙은 함수는 동기적 읽기, 붙지 않은 것은 비동기적 읽기)
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+fs.appendFileSync('notes.txt', ' challenge succeed!!')
