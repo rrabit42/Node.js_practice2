15d0a7c46304c492e3a36bc94c5e06885a2b43f4
rrabit42 <gegiraffe@naver.com>
2020-01-19 20:45:08 +0900

    Importing Node.js Core Modules

---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+const fs = require('fs') // fs module is stored in variable
+//if we use filesystem as name
+
+//this should be filesystem.writeFileSync too.
+//http://nodejs.org/dist/latest-v11.x/docs/api/
+//read official docs
+fs.writeFileSync('notes.txt', 'My name is Andrew.')
