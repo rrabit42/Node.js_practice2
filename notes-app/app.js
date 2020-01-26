bb67b7e3b52e985637c788fdf769e258b32e94d7
rrabit42 <gegiraffe@naver.com>
2020-01-26 13:55:22 +0900

    Argument Parsing with Yargs: Part2 - Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,15 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
+//
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
+
 // Customize yargs version
 yargs.version('1.1.0')
 
@@ -14,10 +23,16 @@ yargs.command({
       describe: 'Note title',
       demandOption: true, // you have to provided it in order for the command, 값이 안주어지면 boolean으로 들어감!
       type: 'string'
+    },
+    body: {
+      describe: 'Note body', //help content에서 보여줌
+      demandOption: true,
+      type: 'string'
     }
   },
   handler: function(argv){
-    console.log('Title: '+argv.title)
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
   }
 })
 
