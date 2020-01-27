99b74509604c5e2913f4687fdc1893ec933f4df7
rrabit42 <gegiraffe@naver.com>
2020-01-27 20:52:48 +0900

    Storing Data with JSON - 2

--- playground/1-json.js
+++ playground/1-json.js
@@ -5,11 +5,6 @@ const book = {
   author: 'Ryan Holiday',
 }
 
-// javascript method that takes in an object or an array or any value for that matter and it returns the Jason string representation.
 const bookJSON = JSON.stringify(book)
-console.log(bookJSON) // double quotes have been added to all property names, JSON is a string not an object 그래서 bookJSON.title 하면 undefined 뜸 물론 book.titleg하면 뜸 book은 object니까
-
-// takes in the JSON string and gives us back the object
-const parseData = JSON.parse(bookJSON)
-console.log(parseData.author) //object니까 property에 접근 가능
+fs.writeFileSync('1-json.json', bookJSON)
 
