63d9242a159ac001f2995ce16f11dcd24c09b713
rrabit42 <gegiraffe@naver.com>
2020-01-27 20:57:57 +0900

    Storing Data with JSON - 3

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,19 @@
 const fs = require('fs')
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-}
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+// }
 
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// what comes back is not a string, it is actually a buffer which is a way for node.js asked to represent binary data
+const dataBuffer = fs.readFileSync('1-json.json')
+// console.log(dataBuffer)
+// 이렇게하면 binary로 보임 따라서 아래와 같이 하기!
+//console.log(dataBuffer.toString())
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+console.log(data.title)
 
