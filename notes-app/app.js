85a3ee252b07d22ee2cc75d9e1a8ce4247d139c2
rrabit42 <gegiraffe@naver.com>
2020-02-24 02:28:46 +0900

    Adding a Note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
-
-//
-// Challenge: Add an option to yargs
-//
-// 1. Setup a body option for the add command
-// 2. Configure a description, make it required, and for it to be a string
-// 3. Log the body value in the handler function
-// 4. Test your work!
-
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -31,8 +22,7 @@ yargs.command({
     }
   },
   handler: function(argv){
-    console.log('Title: ' + argv.title)
-    console.log('Body: ' + argv.body)
+    notes.addNote(argv.title, argv.body)
   }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,50 @@
+const fs = require('fs')
+
 const getNotes = function(){
   return 'Your notes...'
 }
 
-//이거 빼먹어서 틀림!! 잊지 말기~
-module.exports = getNotes
+const addNote = function (title, body) {
+  const notes = loadNotes()
+  //중복되는 data는 추가하지 X -> filter
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title
+  }) //notes array의 subset을 return할 filter -> 그걸 편의상 note라고 이름붙인 것
+
+  //0은 false로 간주되므로
+  if (duplicateNotes.length === 0) {
+    //object를 append(추가됨)
+    notes.push({
+      title: title,
+      body: body
+    })
+    //바뀐 data를 notes에 save
+    saveNotes(notes)
+    console.log('New note added!')
+  }
+  else{
+    console.log('Note title taken!')
+  }
+}
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch(e) {
+    return [] //empty array return
+  }
+}
+
+//여러개 export할 때는 single function 주지 않고 object를 줘서 multiple properties를 접근할 수 있게!
+// property : 진짜 function 이름
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
