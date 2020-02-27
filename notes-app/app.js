d350f821640c14d2ba679a8d7743a0d45eb6b56d
rrabit42 <gegiraffe@naver.com>
2020-02-27 13:52:54 +0900

    Removing a Note - Challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,30 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
+//
+// Challenge 1: Setup command option and function
+//
+// 1. Setup the remove command to take a required "--title" option
+// 2. Create and export a removeNote function from notes.js
+// 3. Call removeNote in remove command handler
+// 4. Have removeNote log the title of the note to be removed
+// 5. Test your work using: node app.js remove --title="some title"
+
+//
+// Challenge 2: Wire up removeNote
+//
+// 1. Load existing notes
+// 2. Use array filter method to remove the matching note (if any)
+// 3. Save the newly created array
+// 4. Test your work with a title that exists and a title that doesn't exist
+
+//
+// Challenge 3: Use chalk to provide useful logs for remove
+//
+// 1. If a note is removed, print "Note removed!" with a green background
+// 2. If no note is removed, print "No note found!" with a red background
+
+
 // Customize yargs version
 yargs.version('1.1.0')
 
@@ -30,8 +54,16 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function(){
-    console.log('Removing the note')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    notes.removeNote(argv.title)
+    console.log("Remove "+argv.title+" note.")
   }
 })
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function(){
   return 'Your notes...'
 }
 
-const addNote = function (title, body) {
+const addNote = function(title, body) {
   const notes = loadNotes()
   //중복되는 data는 추가하지 X -> filter
   const duplicateNotes = notes.filter(function(note) {
@@ -12,7 +13,7 @@ const addNote = function (title, body) {
   }) //notes array의 subset을 return할 filter -> 그걸 편의상 note라고 이름붙인 것
 
   //0은 false로 간주되므로
-  if (duplicateNotes.length === 0) {
+  if(duplicateNotes.length === 0) {
     //object를 append(추가됨)
     notes.push({
       title: title,
@@ -28,7 +29,7 @@ const addNote = function (title, body) {
 }
 
 const saveNotes = function(notes) {
-  const dataJSON = JSON.stringify(notes)
+  const dataJSON = JSON.stringify(notes) // object를 JSON 포맷의 문자열(string)로 바꿔줌
   fs.writeFileSync('notes.json', dataJSON)
 }
 
@@ -36,15 +37,49 @@ const loadNotes = function() {
   try {
     const dataBuffer = fs.readFileSync('notes.json')
     const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
+    return JSON.parse(dataJSON) // JSON 포맷으로 되어 있는 문자열(string)을 JSON 객체로 변환
   } catch(e) {
     return [] //empty array return
   }
 }
+const removeNote = function(title) {
+  const notes = loadNotes()
+  // 배울점 1 + filter 함수 사용법
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title
+  })
+  // 배울점 2: 길이로 비교
+  if(notes.length>notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'))
+  }
+  else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+
+  saveNotes(notesToKeep)
+}
+
+// 내가 짠 코드
+// const removeNote = function(title) {
+//   const notes = loadNotes()
+//   let noTitle = true
+//   const targetNote = notes.filter(function(note) {
+//     if(note.title === title) {
+//       noTitle = false // 이렇게하면 이 변수가 매번 바뀌겠네...
+//       delete note // delete operator deletes only a reference, never an object itself. Since Javascript is garbage collected, you don't need to delete objects themselves - they will be removed when there is no way to refer to them anymore.
+//       const dataJSON = JSON.stringify(notes)
+//       fs.writeFileSync('notes.json', dataJSON, 'utf8', 0o666, 'wx')
+//     }
+//   })
+//   if(noTitle) {
+//     console.log("There is no note having corresponding title")
+//   }
+// }
 
 //여러개 export할 때는 single function 주지 않고 object를 줘서 multiple properties를 접근할 수 있게!
 // property : 진짜 function 이름
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
